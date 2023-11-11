48c56d1f77f9b1045c7f92ed13c54f8e3f53a64e
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-11-11 18:01:24 +0000

    feat: Use a class in .svelte.ts

--- src/components/useLiveblocks.svelte.ts
+++ src/components/useLiveblocks.svelte.ts
@@ -2,27 +2,18 @@ import { enterRoom } from "../liveblocks.config";
 
 // Functions to join/leave a multiplayer room and get real-time Storage
 export function useLiveblocks(roomId: string) {
-  let room = $state();
-  let leave = $state();
-  let storage = $state();
+  class Liveblocks {
+    room = $state();
+    leave = $state();
+    storage = $state();
 
-  async function enter() {
-    const info = enterRoom(roomId);
-    room = info.room;
-    leave = info.leave;
-    storage = (await info.room.getStorage()).root;
+    async enter() {
+      const info = enterRoom(roomId);
+      this.room = info.room;
+      this.leave = info.leave;
+      this.storage = (await info.room.getStorage()).root;
+    }
   }
 
-  return {
-    enter,
-    get leave() {
-      return leave;
-    },
-    get room() {
-      return room;
-    },
-    get storage() {
-      return storage;
-    },
-  };
+  return new Liveblocks();
 }
